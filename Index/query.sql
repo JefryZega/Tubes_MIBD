3bb8fe131e83821b301e20496060ec2acab2ef8e
BMRahardja <6182301062@student.unpar.ac.id>
2025-06-08 02:29:40 +0700

    Add files via upload

--- Index/query.sql
+++ Index/query.sql
@@ -90,6 +90,7 @@ CREATE TABLE dbo.Video (
     userId    INT           NOT NULL,
     thumbnail NVARCHAR(255) NULL,
     subtitle  NVARCHAR(255) NULL,
+	playback  NVARCHAR(255) NULL,
     CONSTRAINT FK_Video_Channel FOREIGN KEY(chnlId) REFERENCES dbo.Channel(chnlId),
     CONSTRAINT FK_Video_User    FOREIGN KEY(userId) REFERENCES dbo.[User](userId)
 );
@@ -150,11 +151,9 @@ GO
 CREATE TABLE dbo.Komen (
     userId       INT           NOT NULL,
     videoId      INT           NOT NULL,
-    tmpstmp      DATETIME      NOT NULL,
     konten       NVARCHAR(MAX) NOT NULL,
-    waktuKomen   TIME          NOT NULL,
     tanggalKomen DATE          NOT NULL,
-    CONSTRAINT PK_Komen          PRIMARY KEY (userId, videoId, tmpstmp),
+    CONSTRAINT PK_Komen          PRIMARY KEY (userId, videoId),
     CONSTRAINT FK_Komen_User     FOREIGN KEY(userId)  REFERENCES dbo.[User](userId),
     CONSTRAINT FK_Komen_Video    FOREIGN KEY(videoId) REFERENCES dbo.Video(videoId)
 );
@@ -184,14 +183,15 @@ select * from Channel
 
 
 
-INSERT INTO dbo.Video (tglUpld, judul, [desc], durasi, status, chnlId, userId, thumbnail, subtitle)
+INSERT INTO dbo.Video (tglUpld, judul, [desc], durasi, status, chnlId, userId, thumbnail, subtitle, playback)
 VALUES
-(GETDATE(), 'My First Vlog', 'Sharing my first day as a content creator', 360, 'up', 1, 1, '../img/thumb1.png', NULL),
-(GETDATE(), 'Tech Review: New Smartphone', 'Unboxing and testing the latest smartphone', 480, 'up', 2, 2, '../img/thumb2.png', NULL),
-(GETDATE(), 'Easy Pasta Recipe', 'Quick dinner recipe you can make in 15 minutes', 600, 'up', 3, 3, '../img/thumb3.png', NULL),
-(GETDATE(), 'Gameplay: Level 1-5 Walkthrough', 'Beginner guide for new players', 720, 'up', 4, 4, '../img/thumb4.png', NULL),
-(GETDATE(), 'Test title', 'Test desc', 100, 'up', 5, 5, '../img/thumb5.png', NULL),
-(GETDATE(), 'Test title', 'Test desc', 100, 'up', 5, 5, '../img/thumb6.png', NULL);
+(GETDATE(), 'My First Vlog', 'Sharing my first day as a content creator', 360, 'up', 1, 1, '../img/thumb1.png', NULL, '../Videos/video_1.mp4'),
+(GETDATE(), 'huh', 'h', 123, 'up', 1, 1, '../img/thumb7.png', NULL, '../Videos/video_2.mp4'),
+(GETDATE(), 'Tech Review: New Smartphone', 'Unboxing and testing the latest smartphone', 480, 'up', 2, 2, '../img/thumb2.png', NULL, '../Videos/video_3.mp4'),
+(GETDATE(), 'Easy Pasta Recipe', 'Quick dinner recipe you can make in 15 minutes', 600, 'up', 3, 3, '../img/thumb3.png', NULL, '../Videos/video_4.mp4'),
+(GETDATE(), 'Gameplay: Level 1-5 Walkthrough', 'Beginner guide for new players', 720, 'up', 4, 4, '../img/thumb4.png', NULL, '../Videos/video_5.mp4'),
+(GETDATE(), 'Test title', 'Test desc', 100, 'up', 5, 5, '../img/thumb5.png', NULL, '../Videos/video_6.mp4'),
+(GETDATE(), 'Test title', 'Test desc', 100, 'up', 5, 5, '../img/thumb6.png', NULL, '../Videos/video_7.mp4');
 
 select * from Video
 
@@ -214,3 +214,23 @@ VALUES
 (3, 1, 420, '2024-06-04', '19:20:00');
 
 select * from [View]
+
+
+
+INSERT INTO Reaksi
+VALUES
+(2, 1, 'like'),
+(2, 2, 'dislike'),
+(1, 1, 'dislike'),
+(1, 3, 'like'),
+(3, 1, 'dislike')
+
+select * from Reaksi
+
+
+
+insert into Komen
+values
+(1, 1, 'waw', GETDATE())
+
+select * from Komen
